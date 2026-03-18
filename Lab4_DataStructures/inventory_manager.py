9a4eccd45664f85c9c4e3e0b9b9671f1fdb10e9e
rchmsnd-maker <rchmsnd@gmail.com>
2026-03-18 21:55:17 +0800

    Complete Lab 4 Exercises

---
+++ Lab4_DataStructures/inventory_manager.py
@@ -0,0 +1,9 @@
+#APPENDIX VARIABLES
+
+SEED_NUM = 4
+STUDENT_MAJOR = "BSECE"
+FAVORITE_ARTIST = "Frank Ocean"
+LOCAL_HAZARD = "FLOOD"
+CONTROL_NUM = max(3, SEED_NUM)
+
+print("Variables initialized successfully.")
